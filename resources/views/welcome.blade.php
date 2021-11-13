fa4feabd4f68ec41ebb39d4742ed77d3e3fbeff1
sandip Tawhare <sandip.tawhare@fyntune.com>
2021-11-13 23:05:58 +0530

    changed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Sandip Tawhare 
+                    Sandip B. Tawhare 
                 </div>
 
                 <div class="links">
